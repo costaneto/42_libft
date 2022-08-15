4cf5cc78b7529f064c33e1c23f8da144f2efb599
costaneto <costaneto50@outlook.pt>
2022-08-15 18:10:57 +0100

    Update 42_header

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/15 15:23:50 by marvin            #+#    #+#             */
-/*   Updated: 2022/08/15 18:08:42 by dneto            ###   ########.fr       */
+/*   Created: 2022/08/15 18:07:23 by dneto             #+#    #+#             */
+/*   Updated: 2022/08/15 18:08:28 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		((char *)dest)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dest);
+	
 }
