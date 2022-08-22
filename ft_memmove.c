57eca6338c2ea335f514f846ab5cb67b78e3599a
costaneto <costaneto50@outlook.pt>
2022-08-22 23:48:19 +0100

    Moved temp into if condition

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:07:23 by dneto             #+#    #+#             */
-/*   Updated: 2022/08/22 23:43:21 by dneto            ###   ########.fr       */
+/*   Updated: 2022/08/22 23:47:46 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*temp;
 
-	temp = malloc(n * sizeof(char));
 	if (ft_strlen(dest) >= n)
 	{
+		temp = malloc(n * sizeof(char));
 		while (n--)
 		{
 			temp[n] = *((unsigned char *)src + n);
 			*(unsigned char *)(dest + n) = temp[n];
 		}
+		free(temp);
 	}
-	free(temp);
 	return (dest);
 }
