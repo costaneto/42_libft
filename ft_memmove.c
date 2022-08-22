68d2b5f92353d362b87668fd71d0c5971ac6e35b
costaneto <costaneto50@outlook.pt>
2022-08-22 23:46:41 +0100

    Worked on memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,27 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:07:23 by dneto             #+#    #+#             */
-/*   Updated: 2022/08/15 18:08:28 by dneto            ###   ########.fr       */
+/*   Updated: 2022/08/22 23:43:21 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	
+	unsigned char	*temp;
+
+	temp = malloc(n * sizeof(char));
+	if (ft_strlen(dest) >= n)
+	{
+		while (n--)
+		{
+			temp[n] = *((unsigned char *)src + n);
+			*(unsigned char *)(dest + n) = temp[n];
+		}
+	}
+	free(temp);
+	return (dest);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 21:12:58 by dneto             #+#    #+#             */
-/*   Updated: 2022/08/15 18:09:28 by dneto            ###   ########.fr       */
+/*   Updated: 2022/08/22 22:39:49 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,21 @@
 // atoi
 void	*ft_bzero(void *s, size_t n);
 // calloc
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isalpha(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
 int		ft_isprint(int c);
-void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
-// memmove
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 // memchr
 // memcmp
 size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *str);
-char	*ft_strncpy(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strdup(const char *str);
 // strnstr
 // strncmp
 int		ft_toupper(int c);
