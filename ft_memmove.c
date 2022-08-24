6908177193e6d01f112f0d754cf25a35e560e88d
costaneto <costaneto50@outlook.pt>
2022-08-24 18:36:44 +0100

    Finished ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,17 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:07:23 by dneto             #+#    #+#             */
-/*   Updated: 2022/08/22 23:47:46 by dneto            ###   ########.fr       */
+/*   Updated: 2022/08/24 18:33:44 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*temp;
 
-	if (ft_strlen(dest) >= n)
+	if (ft_strlen(dest) >= n && ft_strlen(src) >= n)
 	{
 		temp = malloc(n * sizeof(char));
 		while (n--)
