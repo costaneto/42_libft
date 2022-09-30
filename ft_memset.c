0f50d014f71f891c3422afdbc0645b024e10756a
costaneto <dzneto50@gmail.com>
2022-09-30 22:22:13 +0100

    Update timestamp. Remove unused #include's

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,11 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 19:45:21 by dneto             #+#    #+#             */
-/*   Updated: 2022/08/15 18:08:52 by dneto            ###   ########.fr       */
+/*   Updated: 2022/09/30 21:30:28 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-#include <stdio.h>
 
 void	*ft_memset(void *s, int c, size_t n)
 {
