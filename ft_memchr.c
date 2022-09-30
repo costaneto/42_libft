c165e98a6224d24adb67a50ae731a86bc311d31b
costaneto <dzneto50@gmail.com>
2022-09-30 22:23:17 +0100

    Finished ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/30 20:54:13 by dneto             #+#    #+#             */
+/*   Updated: 2022/09/30 22:18:56 by dneto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == ((unsigned char)c))
+			return ((unsigned char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
