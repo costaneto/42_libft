5e329bbd4e0b42aa7f0b079049094934f85ef847
costaneto <dzneto50@gmail.com>
2022-10-01 22:48:36 +0100

    DRY on (unsigned char*)

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 21:51:36 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/01 22:12:30 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/01 22:39:52 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
 	i = 0;
-	while ((((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]) && i < n)
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	while ((str1[i] || str2[i]) && (str1 != str2) && i < n)
 	{
-		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);
