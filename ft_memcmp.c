a8f7525f81c4ef3c3657299efa3bd4df526865bf
costaneto <dzneto50@gmail.com>
2022-10-02 23:22:43 +0100

    Running some tests

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 21:51:36 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/01 23:16:33 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/02 23:21:53 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,32 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	str1 = (const unsigned char *)s1;
 	str2 = (const unsigned char *)s2;
-	while ((str1[i] || str2[i]) && (str1 != str2) && i < n)
+	while ((str1 != str2) && i < n)
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
 		i++;
 	}
+	return (0);
+}
+
+// (str1[i] || str2[i]) && 
+
+
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char s1[] = "abcdefghi";
+	// char s2[] = "abcdzfghi";
+	// char s4[] = "abcdef";
+	// char s3[] = "";
+
+	// s1
+	printf("\n\ns1: \"%s\" (len: %zu)\n______________________________________\n\n", s1, ft_strlen(s1));
+	int res1 = ft_memcmp(s1, s1, 15);
+	int res2 = memcmp(s1, s1, 15);
+	printf("[15] s1: \"%s\"  >>  %d  >>  %d\n\n", s1, res1, res2);
 	return (0);
 }
