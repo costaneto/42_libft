37c148da6770a9433a9d9ef5228e41149c6d7de1
costaneto <dzneto50@gmail.com>
2022-10-22 21:59:28 +0100

    Start ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/22 21:19:55 by dneto             #+#    #+#             */
+/*   Updated: 2022/10/22 21:56:17 by dneto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	if (len > ft_strlen(s[start]))
+		sub = malloc((ft_strlen(s[start]) + 1) * sizeof(char));
+	else
+		sub = malloc((len + 1) * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = 0;
+	return (sub);
+}
