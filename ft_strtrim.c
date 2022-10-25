abba17df7608ad1fcd583a3154bf226508dce06f
Deozete Costa Neto <dneto@c1r13s5.42lisboa.com>
2022-10-25 21:30:57 +0100

    Testing ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,49 +6,58 @@
 /*   By: dneto <dneto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 18:55:29 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/25 18:30:39 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/25 21:29:43 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s, char const *set)
 {
-	int		has_first;
-	int		has_last;
-	size_t	i;
-	size_t	j;
 	char	*trim;
+	size_t	begin;
+	size_t	stop;
+	size_t	i;
 
+	if (!set)
+		return (NULL);
+	begin = 0;
+	stop = ft_strlen(s) - 1;
+	printf(" fine1\n\n");
+	while (s[begin] && ft_strchr(set, s[begin]))
+		begin++;
+	printf(" fine2\n\n");
+	if (begin == ft_strlen(s) - 1)
+	{
+		printf(" beggin == len(s) - 1");
+		return (NULL);
+	}
+	printf(" fine3\n\n");
+	while (s[stop] && ft_strchr(set, s[stop]))
+		stop--;
+	printf(" fine4\n\n");
+	trim = malloc((stop - begin + 2) * sizeof(char));
+	printf(" fine5\n\n");
 	i = 0;
-	has_first = 0;
-	has_last = 0;
-	while (set[i])
-	{
-		if (s1[0] == set[i])
-			has_first = 1;
-		if (s1[ft_strlen(s1) - 1] == set[i])
-			has_last = 1;
-		i++;
-	}
-	i = 0;
-	j = 1;
-	if (has_first && has_last)
-	{
-		trim = malloc((ft_strlen(s1) - 1) * sizeof(char));
-		while (s1[j] && j < ft_strlen(s1) - 1)
-		{
-			trim[i] = s1[j];
-			j++;
-			i++;
-		}
-	}
-	else if (has_first || has_last)
-	{
-		trim = malloc(ft_strlen(s1) * sizeof(char));
-		
-	}
-	else
-		trim = s1;
+	while (begin <= stop)
+		trim[i++] = s[begin++];
+	printf(" fine6\n\n");
+	trim[i] = 0;
+	printf(" fine7\n\n");
 	return (trim);
+}
+
+
+int	main(void)
+{
+	printf("\n\n");
+	char s[] = ".........ooooo.....ooooo";
+	char set[] = "osdas..";
+	char *test;
+
+	test = ft_strtrim(s, set);
+	printf("\n\n %p\n\n", test);
+
+	return (0);
 }
