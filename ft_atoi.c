75ddae4c1b24db716a9c346bd27a822e5972ff4a
Deozete Costa Neto <dneto@c1r13s1.42lisboa.com>
2022-10-28 19:25:04 +0100

    Testing atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:33:51 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/28 18:14:02 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/28 19:19:49 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,14 @@ int	ft_atoi(const char *str)
 	i = 0;
 	signal = 1;
 	res = 0;
-	if (str[0] == '-' || str[0] == '+')
+	if (!str)
+		return (0);
+	while (str[i] >= 0 && str[i] <= 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
 		i++;
-		if (str[0] == '-')
+		if (str[i] == '-')
 			signal *= -1;
 	}
 	while (str[i] && str[i] > 47 && str[i] < 58)
@@ -35,16 +39,16 @@ int	ft_atoi(const char *str)
 	return (res * signal);
 }
 
-#include <stdlib.h>
-#include <stdio.h>
+// #include <stdlib.h>
+// #include <stdio.h>
 
-int main()
-{
-    char str1[] = "fasfa3sfsa\0asda1";
-    char str2[] = "++1";
-    char str3[] = "-+1";
+// int main()
+// {
+//     char str1[] = "1";
+//     char str2[] = "++1";
+//     char str3[] = "-+1";
 
-    printf("\n\nstr1: %d    str2: %d    str3: %d\n\n", atoi(str1), atoi(str2), atoi(str3));
-    printf("\n\nstr1: %d    str2: %d    str3: %d\n\n", ft_atoi(str1), ft_atoi(str2), ft_atoi(str3));
-    return (0);
-}
+//     printf("\n\nstr1: %d    str2: %d    str3: %d\n\n", atoi(str1), atoi(str2), atoi(str3));
+//     printf("\n\nstr1: %d    str2: %d    str3: %d\n\n", ft_atoi(str1), ft_atoi(str2), ft_atoi(str3));
+//     return (0);
+// }
