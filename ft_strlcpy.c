36f4aea2837b6fcae76ff40295cecf63c68bafbe
Deozete Costa Neto <dneto@c2r11s4.42lisboa.com>
2022-10-28 17:36:21 +0100

    Added action for size = 0 case

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 10:01:09 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/27 17:07:44 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/28 17:17:16 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (!size)
+		return (ft_strlen(src));
 	while (src[i] && i < size - 1)
 	{
 		dest[i] = src[i];
