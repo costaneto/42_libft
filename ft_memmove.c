096b9052f391d45fbad85d9aaee000854fe7153e
costaneto <dzneto50@gmail.com>
2022-10-29 20:55:24 +0100

    [+] ft_memmove.c unit_test pass

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,36 +6,32 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:07:23 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/27 17:11:06 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/29 20:52:43 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *d, const void *s, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*s;
+	unsigned char	*d;
 
-	i = 0;
-	if (s && d)
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	if (!s && !d)
+		return (NULL);
+	if (s >= d)
 	{
-		if (s >= d)
-		{
-			while (i < n)
-			{
-				((unsigned char *)d)[i] = ((unsigned char *)s)[i];
-				i++;
-			}
-		}
-		else
-		{
-			while (n > 0)
-			{
-				((unsigned char *)d)[n - 1] = ((unsigned char *)s)[n - 1];
-				n--;
-			}
-		}
-		return (d);
+		i = -1;
+		while (++i < n)
+			d[i] = s[i];
 	}
-	return (NULL);
+	else
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	return (dest);
 }
