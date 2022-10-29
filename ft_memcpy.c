4a6ac84bb16beadbb509fe075f52f22054d8f52b
costaneto <dzneto50@gmail.com>
2022-10-29 20:24:37 +0100

    [+] ft_memcpy.c unit_test pass

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 15:23:50 by marvin            #+#    #+#             */
-/*   Updated: 2022/08/31 17:12:21 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/29 20:21:41 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dest && !src)
+		return (NULL);
 	while (i < n)
 	{
 		((char *)dest)[i] = ((unsigned char *)src)[i];
