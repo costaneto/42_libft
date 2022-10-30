c477126556b718fb8219f56ca5e0364230b83045
costaneto <dzneto50@gmail.com>
2022-10-30 18:07:18 +0000

    [-] unit_test fail (no seg crash)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 20:28:52 by dneto             #+#    #+#             */
-/*   Updated: 2022/10/27 17:07:41 by dneto            ###   ########.fr       */
+/*   Updated: 2022/10/30 18:05:33 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,16 @@ char	*ft_strnstr(const char	*big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
-	if (!ft_strlen(little))
+	if (!*little)
 		return ((char *)big);
-	while (i < len)
+	i = 0;
+	while (big && big[i] && i < len)
 	{
-		if (big[i] == little[0])
-		{
-			j = 0;
-			while (big[i + j] == little[j] && i + j < len)
-				j++;
-			if (j == ft_strlen(little))
-				return ((char *)big + i);
-		}
+		j = 0;
+		while ((big[i + j] == little[j]) && (i + j < len) && (big[i + j] != 0))
+			j++;
+		if (little[j] == 0)
+			return ((char *)(big + i));
 		i++;
 	}
 	return (NULL);
