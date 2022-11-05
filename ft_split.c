60ed4aac083c0109c512deda7fd4754b27dd1d4f
costaneto <dzneto50@gmail.com>
2022-11-05 14:38:42 +0000

    [ - ] unit-test fail

--- ft_split.c
+++ ft_split.c
@@ -6,37 +6,38 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:46:24 by dneto             #+#    #+#             */
-/*   Updated: 2022/11/05 12:38:26 by dneto            ###   ########.fr       */
+/*   Updated: 2022/11/05 14:37:53 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 static int	ft_counter(char const *s, char sep)
 {
-	int		substr_count;
+	int	substr_count;
+	int	i;
 
 	substr_count = 0;
-	if (*s != sep)
+	i = 0;
+	if (s[i] != sep)
 		substr_count = 1;
-	while (*s)
+	while (s[i])
 	{
-		if (*s != sep && *(s - 1) == sep)
+		if (i != 0 && s[i] != sep && s[i - 1] == sep)
 			substr_count++;
-		s++;
+		i++;
 	}
 	return (substr_count);
 }
 
-static char	*ft_getsubstr(char *s, char sep)
+static char	*ft_getsubstr(char const *s, char sep)
 {
 	int		i;
 	char	*substr;
 
 	if (!s)
 		return (NULL);
-	while (*s && *s == sep)
-		s++;
 	i = 0;
 	while (s[i] && s[i] != sep)
 		i++;
@@ -50,13 +51,14 @@ static char	*ft_getsubstr(char *s, char sep)
 char	**ft_split(char const *s, char sep)
 {
 	char	**arr;
-	char	*substr;
+	char	*temp;
 	int		i;
 	int		counter;
 
 	if (!s)
 		return (NULL);
 	counter = ft_counter(s, sep);
+	printf("\n\n%d\n\n", counter);
 	arr = malloc(counter * sizeof(char *));
 	if (!arr)
 		return (NULL);
@@ -65,11 +67,20 @@ char	**ft_split(char const *s, char sep)
 	{
 		while (*s && *s == sep)
 			s++;
-		substr = ft_getsubstr(s, sep);
+		temp = ft_getsubstr(s, sep);
+		arr[i++] = temp;
+		free(temp);
 		while (*s && *s != sep)
 			s++;
-		arr[i++] == &substr;
-		free(substr);
 	}
 	return (arr);
+}
+
+
+int main()
+{
+	char *string = "      split   ggg    this for   me  !       ";
+	char **result = ft_split(string, ' ');
+	printf("\n\n%s\n\n", result[0]);
+	return (0);
 }
