459d8662ec32b68ca0ab71150fd482af6a79b0b6
costaneto <dzneto50@gmail.com>
2022-11-05 18:20:03 +0000

    Added ft_split

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 21:12:58 by dneto             #+#    #+#             */
-/*   Updated: 2022/11/01 11:43:26 by dneto            ###   ########.fr       */
+/*   Updated: 2022/11/05 18:17:42 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ int		ft_tolower(int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
