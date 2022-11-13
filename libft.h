1cf65b6e0ba76a73f2f8cb43633904fa101dcfe7
costaneto <dzneto50@gmail.com>
2022-11-13 17:34:16 +0000

    [+] ft_lstadd_front PASS

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 21:12:58 by dneto             #+#    #+#             */
-/*   Updated: 2022/11/12 18:24:41 by dneto            ###   ########.fr       */
+/*   Updated: 2022/11/13 14:01:27 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
